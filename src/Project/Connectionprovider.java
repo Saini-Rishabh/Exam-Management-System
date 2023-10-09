e77c6c02d99cf8d12250bb1c0e76721a4c0d6c29
Saini-Rishabh <saini.rishabh.2016955@gmail.com>
2023-10-09 23:56:15 +0530

    Initial commit

---
+++ src/Project/Connectionprovider.java
@@ -0,0 +1,26 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt  to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package Project;
+import java.sql.*;
+/**
+ *
+ * @author Administrator
+ */
+public class Connectionprovider {
+    public static Connection getCon()
+    {
+        try{
+            Class.forName("com.mysql.jdbc.Driver");
+            Connection con=DriverManager.getConnection("jdbc:mysql://localhost:3306/qems","root","Rishabh@28");
+            return con;
+        }
+        catch(Exception e)
+        {
+            return null;
+            
+        }
+    }
+    
+}
